55c355442a501311310739b67b614463d5b41d39
Little Jacob <joelnoumbi28@gmail.com>
2023-04-04 15:13:51 +0100

    navbar && sidebar on the way ⏫

---
+++ components/Discover/index.tsx
@@ -0,0 +1,32 @@
+import { topics } from "@/utils/constants";
+import Link from "next/link";
+
+type Props = {};
+
+const Discover = (props: Props) => {
+  const topicStyle =
+    "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+
+  const activeTopStyle = "";
+  return (
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold mx-3 mb-3 mt-4 hidden xl:block">
+        Popular Topics
+      </p>
+      <div className="flex md:flex-row flex-col gap-3 flex-wrap">
+        {topics.map((item) => (
+          <Link href={`/topic=${item.name}`} key={item.name}>
+            <div className={topicStyle}>
+              <span className="font-bold text-2xl xl:text-md">{item.icon}</span>
+              <span className="font-medium text-md hidden xl:block capitalize">
+                {item.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+// next
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-type Props = {}
+// icons
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <header>Navbar</header>
-  )
-}
+    <header className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            alt=""
+            className="cursor-pointer"
+            src="/tiktik.png"
+            width={200}
+            height={200}
+          />
+        </div>
+      </Link>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
