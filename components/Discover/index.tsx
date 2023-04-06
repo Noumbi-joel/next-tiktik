01008a0d22c37cce3bcabba96b682482db17d5b1
Little Jacob <joelnoumbi28@gmail.com>
2023-04-06 23:59:13 +0100

    color

--- components/Discover/index.tsx
+++ components/Discover/index.tsx
@@ -7,7 +7,8 @@ const Discover = (props: Props) => {
   const topicStyle =
     "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
 
-  const activeTopStyle = "";
+  const activeTopStyle =
+    "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold mx-3 mb-3 mt-4 hidden xl:block">
